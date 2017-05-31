41b1d215bc333167c9fc99dcebdfa6c501b04dc2
real@mpower-social.com <real@mpower-social.com>
2017-05-31 12:18:04 +0600

    Full code with Test class and Configuring Jacoco

---
+++ src/main/java/real/abc/Student.java
@@ -0,0 +1,28 @@
+package real.abc;
+
+/**
+ * Created by real on 5/11/17.
+ */
+public class Student {
+    private int id;
+    private String name;
+    private int score;
+
+    public Student(int id, String name, int score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
